1e61ab5bcb0ce30c4a6bb4fced4b74e0de917d0c
Okan Vurdu <vurduo@gmail.com>
2015-10-10 21:59:32 +0300

    Stiller yeniden düzenlendi.

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,3 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 var req = new XMLHttpRequest();
 req.open(
   "GET",
@@ -11,16 +7,16 @@ req.open(
 
 function showArticles() {
   var articles = req.responseXML.getElementsByTagName("item"), i, atag, linkURL, div;
+  div = document.createElement('div');
   for (i = 0; i < 5; i++) {
-    div = document.createElement('div');
     atag = document.createElement("a");
     linkURL = articles[i].getElementsByTagName("link").item(0).firstChild.nodeValue;
     atag.href = linkURL;
     atag.target = "_blank";
     atag.appendChild(articles[i].getElementsByTagName("title").item(0));
     div.appendChild(atag);
-    document.body.appendChild(div);
   }
+  document.body.appendChild(div);
 }
 req.onload = showArticles;
 req.send(null);
